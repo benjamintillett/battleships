ea313c4fc41ceae213f117642cb6d3a3cf783446
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-26 17:28:49 +0100

    Giving the ship a default health which is able to change

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,11 +1,32 @@
 class Ship
 
+  def initialize
+  	@health = 2
+  	@sunk = false
+  end
+
+  def health
+  	@health
+  end
+
+  def sustaindamage!
+  	@health -= 1
+  end
+
   def sunk?
     @sunk
   end
 
   def sink!
     @sunk = true
+  end
+
+   def locations
+  	@locations
+   end
+
+  def place_at!(coordinates: [])
+    @locations = coordinates
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -15,4 +15,35 @@ let(:ship) { Ship.new }
     #changing sunk status from false > true
   end
 
+  it "Should initialize with a default amount of health" do
+  	expect(ship.health).to eq 2
+  end
+
+  it "Should know when it has been damaged" do
+  	ship.sustaindamage!
+  	expect(ship.health).to eq 1
+  end
+
+
+  # it "should know how many hits it has taken" do
+  # 	ship.sustaindamage(1)
+  # 	expect ship.health to eq 
+  # end
+
+  # it "should know "
+
+  # it "Should be able to store a location" do
+  # 	ship.place_at!(coordinates: [:A1, :A2])
+  # 	expect(ship.locations).to eq [:A1, :A2]
+  # end
+
+  # it "Should be able to store a location" do
+  # 	ship.place_at!(coordinates: [:B1, :B2])
+  # 	expect(ship.locations).to eq [:B1, :B2]
+  # end
+
+  # it "Should know if it has been hit" do 
+  # 	ship.
+
+
 end
