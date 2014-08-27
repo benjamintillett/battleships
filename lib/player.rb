99ed7e2cffb4a2df984e8d96e01095b843d9487e
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-27 18:42:00 +0100

    Adding test for Player#shoot_cell

--- lib/player.rb
+++ lib/player.rb
@@ -2,13 +2,17 @@ require 'board'
 
 class Player 
 
-	attr_reader :board, :name
+	attr_reader :name
+	attr_accessor :board
 	
-	def initialize(name= "NoName")
-		@board = Board.new({})
+	def initialize(name= "NoName", board)
+		@board = board
 		@name = name
 	end
 
+	def add_ship_to(cell, ship)
+		board.add_ship_to(cell, ship)
+	end
 
 
 end
