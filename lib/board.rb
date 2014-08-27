14e772b74d9827da7e681440f4b29ab0799b7f0b
benjamintillett <benjamin.tillett@gmail.com>
2014-08-27 14:02:43 +0100

    add #in_play? #start_playing and @health to board class

--- lib/board.rb
+++ lib/board.rb
@@ -7,14 +7,29 @@ class Board
 	def initialize(cell_hash)
 		@cells = cell_hash
 		@health = 0
+		@in_play = false
+
 	end
 
 	def add_ship_to(cell_location, ship)
 		@cells[cell_location].add_ship!(ship)
+		@health +=1
 	end
 
 	def shoot_cell(cell_location)
+		raise RuntimeError , "Game has not started!!!" if @in_play == false
 		@cells[cell_location].shoot!
+		@health -=1
 	end
+
+	def in_play?
+		@in_play
+	end
+
+	def start_playing
+		@in_play = true
+	end
+
+
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,8 +9,6 @@ describe Board  do
 	let(:cell_hash) { {:A1 => cell1, :A2 => cell2 , :B1 => cell3, :B2 => cell4 } }
 
 
-	
-
 
 	it "can be initialized with an hash of cells" do 
 		board = Board.new(cell_hash)
@@ -18,20 +16,50 @@ describe Board  do
 	end
 
 	let(:board) { Board.new(cell_hash) }		
+	let(:ship) { double :ship }
 
 	it "initialized with 0 health" do
 		expect(board.health).to eq 0
 	end
 
 	it "can add a ship to a cell" do 
-		ship = double :ship
 		expect(cell1).to receive(:add_ship!).with(ship)  
 		board.add_ship_to(:A1,ship)
 	end
 
+	it "health increments by 1 if ship has been added to 1 cell" do 
+		allow(cell1).to receive(:add_ship!).with(ship)  
+		board.add_ship_to(:A1,ship)
+		expect(board.health).to eq 1 
+	end
+
 	it "can shoot at a cell" do 
+		board.start_playing
 		expect(cell2).to receive(:shoot!)
 		board.shoot_cell(:A2)
 	end
+	it "decrements its health by one if cell containing a ship is shot" do 
+		board.start_playing
+		allow(cell1).to receive(:add_ship!).with(ship)
+		allow(cell1).to receive(:shoot!)  
+		board.add_ship_to(:A1,ship)	
+		board.shoot_cell(:A1)
+		expect(board.health).to eq 0
+	end
+
+	it "is not in play when initialized" do 
+		expect(board).not_to be_in_play
+	end
+
+	it "is in play after it has been started" do
+		board.start_playing
+		expect(board).to be_in_play
+	end
+
+	it "wont let you shoot if game is not in play" do 
+		allow(cell1).to receive(:shoot!)
+		expect{ board.shoot_cell(:A1) }.to raise_error "Game has not started!!!"
+	end
+
 
 end
