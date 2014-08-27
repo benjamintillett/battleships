966bc93250c324a8699de633337ed9805dd13f08
benjamintillett <benjamin.tillett@gmail.com>
2014-08-27 16:21:55 +0100

    Board: add #game_over #decrement_health and refactor specs

--- lib/board.rb
+++ lib/board.rb
@@ -5,21 +5,24 @@ class Board
 	attr_reader :health, :cells
 
 	def initialize(cell_hash)
-		@cells = cell_hash
+		@cells = cell_hash 
 		@health = 0
 		@in_play = false
+		@game_over = false
 
 	end
 
 	def add_ship_to(cell_location, ship)
+		raise RuntimeError ,"Game is in play, you can no longer place ships" if in_play?
 		@cells[cell_location].add_ship!(ship)
 		@health +=1
+		self
 	end
 
 	def shoot_cell(cell_location)
 		raise RuntimeError , "Game has not started!!!" if @in_play == false
 		@cells[cell_location].shoot!
-		@health -=1
+		decrement_health
 	end
 
 	def in_play?
@@ -30,6 +33,15 @@ class Board
 		@in_play = true
 		self
 	end
+
+	def game_over?
+		@game_over
+	end
+
+	def decrement_health
+		@health -=1
+		@game_over = true if @health == 0 
+	end 
 
 
 
