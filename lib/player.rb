f0df118eb32eb4ee5639d2319b977b2c9c6328f8
leopoldkwok <leopoldkwok@gmail.com>
2014-08-28 13:24:53 +0100

    added shoot_ceLL-on_my_board and has a game, can shoot a cell location on my board

--- lib/player.rb
+++ lib/player.rb
@@ -2,21 +2,26 @@ require 'board'
 
 class Player 
 
-	attr_reader :name
+	attr_reader :name, :game
 	attr_accessor :board
 	
-	def initialize(name= "NoName", board )
+	def initialize(name= "NoName", board, game)
 		@board = board
 		@name = name
+		@game = game
 	end
 
 	def add_ship_to(cell, ship)
 		board.add_ship_to(cell, ship)
 	end
 
-	def shoot_cell(cell)
+	def shoot_cell_on_my_board(cell) #receiving opponents shots
 		board.shoot_cell(cell)
 	end
+
+	# def shoot_cell_on_opponents_board(cell_location)
+	# 	game.shoot_opponents_cells(cell)
+	# end
 
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,19 +7,19 @@ describe Player do
 	context "On initialization:" do 
 
 		it "has a board" do
-			player = Player.new('', board)
+			player = Player.new('', board, game)
 			expect(player.board.class).to eq Board
 		end
 
 		it "Has a name" do
-			player = Player.new("Ben", board)
+			player = Player.new("Ben", board, game)
 			expect(player.name).to eq "Ben"  
 		end
 		
-		# it "Has a game" do 
-		# 	player = Player.new("Ben", board, game)
-		# 	expect(player.game.class).to eq game
-		# end
+		it "Has a game" do 
+			player = Player.new("Ben", board, game)
+			expect(player.game.class).to eq Game
+		end
 
 	end
 
@@ -31,13 +31,10 @@ describe Player do
 		player.add_ship_to(:A1,ship)
 	end 
 
-	# it "can shoot a cell location" do
-	# 	expect(player.board).to receive(:shoot_cell).with(:A1)
-	# 	player.shoot_cell(:A1)
-	# end
-
-
-
+	it "can shoot a cell location on my board" do
+	 	expect(player.board).to receive(:shoot_cell).with(:A1)
+	 	player.shoot_cell_on_my_board(:A1)
+	 end
 
 
 
